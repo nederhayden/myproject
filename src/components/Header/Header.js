7d9fc1275bb6357b4589213f3661ba431b101a26
Neder <nedersimoes@gmail.com>
2021-07-12 13:26:26 -0400

    implementing responsive layout

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,16 +5,36 @@ import logo from "../../assets/logo.png";
 import "./header.scss";
 
 export default function Header() {
+  let show = true;
+
+  const menuSection = document.querySelector(".menu-section");
+  const menuToggle = document.querySelector(".menu-toggle");
+
+  menuToggle.addEventListener("click", () => {
+    document.body.style.overflow = show ? "hidden" : "initial";
+    menuSection.classList.toggle("on", show);
+    show = !show;
+  });
+
   return (
     <div className="header">
       <div className="logo">
         <img src={logo} alt="Logo" />
         <strong>Developers</strong>
       </div>
-      <div className="redes-sociais">
-        <Link to="sem link ainda">Front-End</Link>
-        <Link to="sem link ainda">Back-End</Link>
-        <Link to="sem link ainda">Fullstack</Link>
+      <div className="menu-section">
+        <div className="menu-toggle">
+          <div className="one"></div>
+          <div className="two"></div>
+          <div className="three"></div>
+        </div>
+        <div className="types-dev">
+          <ul>
+            <Link to="sem link ainda">Front-End</Link>
+            <Link to="sem link ainda">Back-End</Link>
+            <Link to="sem link ainda">Fullstack</Link>
+          </ul>
+        </div>
       </div>
     </div>
   );
