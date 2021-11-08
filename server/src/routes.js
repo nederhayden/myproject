605a6d632c1943859ff13c2094bbcd574f318401
Neder <nedersimoes@gmail.com>
2021-11-08 15:19:51 -0400

    delete method get and delete

--- server/src/routes.js
+++ server/src/routes.js
@@ -2,24 +2,18 @@ const routes = require("express").Router();
 const multer = require("multer");
 const multerConfig = require("./config/multer");
 
-routes.get("/posts", async (req, res) => {
-  try {
-    res.json(req.file);
-  } catch (error) {
-    console.log(error);
-  }
-});
-
 routes.post("/posts", multer(multerConfig).single("file"), async (req, res) => {
+  const { originalname: name, size, filename: key } = req.file;
+  const post = {
+    name,
+    size,
+    key,
+  };
   try {
-    res.json("Feito o upload da imagem");
+    return res.json(post);
   } catch (error) {
     console.log(error);
   }
-});
-
-routes.delete("/posts/:id", async (req, res) => {
-  return res.send();
 });
 
 module.exports = routes;
